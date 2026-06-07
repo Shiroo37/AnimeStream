5341aaf53683e3d572b55929bedae524bdafa501
Naufal Sausan Su'daa <naufalsausan37@gmail.com>
2026-06-07 17:42:38 +0700

    Update Extractors.kt

--- LayarKaca/src/main/kotlin/com/layarkaca/Extractors.kt
+++ LayarKaca/src/main/kotlin/com/layarkaca/Extractors.kt
@@ -3,11 +3,49 @@ package com.layarkaca
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.extractors.Filesim
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.M3u8Helper
-import com.lagradost.cloudstream3.utils.getQualityFromName
-import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.*
+
+// Extractor untuk playeriframe.sbs
+open class Playeriframe : ExtractorApi() {
+    override val name = "Playeriframe"
+    override val mainUrl = "https://playeriframe.sbs"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val response = app.get(url, referer = referer, allowRedirects = true)
+        val doc = response.document
+        val text = doc.html()
+
+        // Cari m3u8 atau mp4 di script
+        val videoRegex = Regex("""['"](https?://[^'"]+\.(?:m3u8|mp4)[^'"]*)['"']""")
+        for (match in videoRegex.findAll(text)) {
+            val videoUrl = match.groupValues[1]
+            // Skip video iklan
+            if (videoUrl.contains("stopjudi") || videoUrl.contains("donasi")) continue
+            val isM3u8 = videoUrl.contains("m3u8")
+            M3u8Helper.generateM3u8(name, videoUrl, url).forEach(callback)
+            if (!isM3u8) {
+                callback.invoke(
+                    newExtractorLink(name, name, videoUrl, ExtractorLinkType.VIDEO) {
+                        this.referer = url
+                        this.quality = Qualities.Unknown.value
+                    }
+                )
+            }
+        }
+
+        // Fallback: cek iframe di dalamnya
+        val innerIframe = doc.selectFirst("iframe[src]")?.attr("src") ?: return
+        if (innerIframe.startsWith("http") && !innerIframe.contains("playeriframe")) {
+            loadExtractor(innerIframe, url, subtitleCallback, callback)
+        }
+    }
+}
 
 open class Emturbovid : ExtractorApi() {
     override val name = "Emturbovid"
@@ -15,17 +53,14 @@ open class Emturbovid : ExtractorApi() {
     override val requiresReferer = true
 
     override suspend fun getUrl(
-            url: String,
-            referer: String?,
-            subtitleCallback: (SubtitleFile) -> Unit,
-            callback: (ExtractorLink) -> Unit
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
     ) {
         val response = app.get(url, referer = referer)
-        val m3u8 =
-                Regex("[\"'](.*?master\\.m3u8.*?)[\"']")
-                        .find(response.text)
-                        ?.groupValues
-                        ?.getOrNull(1)
+        val m3u8 = Regex("[\"'](.*?master\\.m3u8.*?)[\"']")
+            .find(response.text)?.groupValues?.getOrNull(1)
         M3u8Helper.generateM3u8(name, m3u8 ?: return, mainUrl).forEach(callback)
     }
 }
@@ -36,44 +71,34 @@ open class Hownetwork : ExtractorApi() {
     override val requiresReferer = true
 
     override suspend fun getUrl(
-            url: String,
-            referer: String?,
-            subtitleCallback: (SubtitleFile) -> Unit,
-            callback: (ExtractorLink) -> Unit
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
     ) {
         val id = url.substringAfter("id=")
-        val res =
-                app.post(
-                                "$mainUrl/api.php?id=$id",
-                                data =
-                                        mapOf(
-                                                "r" to "https://playeriframe.sbs/",
-                                                "d" to "stream.hownetwork.xyz",
-                                        ),
-                                referer = url,
-                                headers = mapOf("X-Requested-With" to "XMLHttpRequest")
-                        )
-                        .parsedSafe<Sources>()
+        val res = app.post(
+            "$mainUrl/api.php?id=$id",
+            data = mapOf(
+                "r" to "https://playeriframe.sbs/",
+                "d" to "stream.hownetwork.xyz",
+            ),
+            referer = url,
+            headers = mapOf("X-Requested-With" to "XMLHttpRequest")
+        ).parsedSafe<Sources>() ?: return
 
-        res?.data?.map {
+        for (item in res.data) {
             callback.invoke(
-				newExtractorLink(
-                    name,
-                    name,
-                    it.file
-                ){
-					this.referer = url
-					this.quality = getQualityFromName(it.label)
-				}                    
+                newExtractorLink(name, name, item.file, ExtractorLinkType.VIDEO) {
+                    this.referer = url
+                    this.quality = getQualityFromName(item.label)
+                }
             )
         }
     }
 
-    data class Sources(val data: ArrayList<Data>) {
-        data class Data(
-                val file: String,
-                val label: String?,
-        )
+    data class Sources(val data: List<Data>) {
+        data class Data(val file: String, val label: String?)
     }
 }
 
@@ -81,4 +106,3 @@ class FileMoon : Filesim() {
     override val name = "FileMoon"
     override var mainUrl = "https://filemoon.sx"
 }
-
