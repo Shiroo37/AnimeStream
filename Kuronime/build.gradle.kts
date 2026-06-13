2ebf35ce78c17b33d70b8692a65b99200e05f44a
Naufal Sausan Su'daa <naufalsausan37@gmail.com>
2026-06-13 15:15:01 +0700

    Update build.gradle.kts

--- Kuronime/build.gradle.kts
+++ Kuronime/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 21
+version = 25
 
 
 cloudstream {
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-     authors = listOf("Hexated", "TeKuma25")
+     authors = listOf("Nauflsan")
 
     /**
      * Status int as the following:
